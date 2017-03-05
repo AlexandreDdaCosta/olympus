4d811e31e05c8007af1375f728162c9547d74dfd
Alex da Costa <alex_investor@yahoo.com>
2017-03-05 11:59:12 -0500

    Working Django build

--- service/django/interface/settings.py
+++ service/django/interface/settings.py
@@ -1,30 +1,70 @@
 import os
 
-SECRET_KEY = 'jscfofujekl'
-BASE_DIR = os.path.dirname(__file__)
+SECRET_KEY = 'b=3o$gi)i=sx!$%3_31-%053i2ct7wwbk7swd1ey7vtj71h3=d'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ID = 1
-
-# Application definition
+DEBUG = True
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes.models.ContentType',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
-MEDIA_ROOT = BASE_DIR+'media/'
-MEDIA_ROOT_ADMIN = BASE_DIR+'media-admin/'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MEDIA_ROOT = '/srv/www/django/interface/media'
+MEDIA_ROOT_ADMIN = '/srv/www/django/interface/media-admin'
 MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 ROOT_URLCONF = 'interface.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 WSGI_APPLICATION = 'interface.wsgi.application'
-
-# Hack that ensures all incoming requests are seen as HTTPS
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO',None)
-
-# Enable access to session cookies to Javascript
-
-SESSION_COOKIE_HTTPONLY = False
-
-# Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/New_York'
@@ -32,7 +72,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
+# Hack that ensures all incoming requests are seen as HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO',None)
 
-STATIC_URL = '/static/'
+# Enable access to session cookies to Javascript
+SESSION_COOKIE_HTTPONLY = False
--- service/django/interface/wsgi.py
+++ service/django/interface/wsgi.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import os, sys
 sys.path.append('/usr/local/lib/python3.4/dist-packages')
-import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interface.settings")
-django.setup()
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interface.settings")
 application = get_wsgi_application()
