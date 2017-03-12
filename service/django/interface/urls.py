d5b11fe03464513a8a0261a66497a21b77f3e55d
Alex da Costa <alex_investor@yahoo.com>
2017-03-11 23:42:34 -0500

    Bad url for welcome

--- service/django/interface/urls.py
+++ service/django/interface/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('interface.apps.welcome.urls'), name='welcome'),
+    url(r'^/', include('interface.apps.welcome.urls'), name='welcome'),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('interface.apps.blog.urls'), name='blog'),
 ]
