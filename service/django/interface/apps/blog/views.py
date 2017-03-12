ee3008a69a6625b9553166fb7ace5fa71dcc56a9
Alex da Costa <alex_investor@yahoo.com>
2017-03-11 23:08:00 -0500

    Debugging blog

--- service/django/interface/apps/blog/views.py
+++ service/django/interface/apps/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-class FrontPage():
+class FrontPage(TemplateView):
 
     def get(self, errors=False, *args, **kwargs):
         context = {}
