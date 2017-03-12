1febeca3963df56982c23e4e084f9554c4896fb0
Alex da Costa <alex_investor@yahoo.com>
2017-03-11 23:50:19 -0500

    URL bug

--- service/django/interface/urls.py
+++ service/django/interface/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^/', include('interface.apps.welcome.urls'), name='welcome'),
+    url(r'^$', RedirectView.as_view(url='/welcome/')),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('interface.apps.blog.urls'), name='blog'),
+    url(r'^welcome/', include('interface.apps.welcome.urls'), name='welcome'),
 ]
