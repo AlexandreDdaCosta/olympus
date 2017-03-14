5e492d955e2fcdfeb9dabae7571c9370dbb645f1
Alex da Costa <alex_investor@yahoo.com>
2017-03-13 21:40:39 -0400

    Debug settings

--- service/django/interface/settings.py
+++ service/django/interface/settings.py
@@ -40,12 +40,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 ROOT_URLCONF = 'interface.urls'
 TEMPLATES = [
     {
