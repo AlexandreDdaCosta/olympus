e003b9f841196dffdb144913da7d415448b19129
Alex da Costa <alex_investor@yahoo.com>
2017-03-13 21:14:26 -0400

    firewall

--- service/salt/fs/stage/develop/services/frontend/files/startserver.py
+++ service/salt/fs/stage/develop/services/frontend/files/startserver.py
@@ -2,5 +2,5 @@
 
 from subprocess import Popen
  
-cmd_str = 'sudo -u uwsgi /usr/bin/python3 /srv/www/django/manage.py runserver 0.0.0.0:8000 1>>/var/log/devserver.log 2>>/var/log/devserver.log &'
+cmd_str = 'sudo -u uwsgi /usr/bin/python3 /srv/www/django/manage.py runserver 1>>/var/log/devserver.log 2>>/var/log/devserver.log &'
 proc = Popen([cmd_str], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
