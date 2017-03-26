d54c16b9d862bbfb1610b38b10dc4ccb3c2bf50f
Alex da Costa <alex_investor@yahoo.com>
2017-03-26 02:32:44 -0400

    Template work

--- service/django/interface/urls.py
+++ service/django/interface/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/welcome/')),
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('interface.apps.blog.urls'), name='blog'),
-    url(r'^welcome/', include('interface.apps.welcome.urls'), name='welcome'),
+    url(r'^blog/', include('interface.apps.blog.urls')),
+    url(r'^welcome/', include('interface.apps.welcome.urls')),
 ]
