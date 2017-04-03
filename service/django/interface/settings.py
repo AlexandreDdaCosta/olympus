2fde6554c2c4a499d67976222f4d2ab70a84a75a
Alex da Costa <alex_investor@yahoo.com>
2017-04-03 00:29:08 -0400

    Adding blog to apps

--- service/django/interface/settings.py
+++ service/django/interface/settings.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
