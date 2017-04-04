3343fed877dffc25a11911ed2ba7879965788962
Alex da Costa <alex_investor@yahoo.com>
2017-04-04 00:28:14 -0400

    Debugging verifyuser

--- service/django/interface/settings.py
+++ service/django/interface/settings.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'interface',
     'interface.apps.blog',
 ]
 MIDDLEWARE = [
