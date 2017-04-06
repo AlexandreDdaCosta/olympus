80e5714875ec2e32160afdd7edf7c127bfb8362b
Alex da Costa <alex_investor@yahoo.com>
2017-04-05 23:14:11 -0400

    Deprecation bug

--- service/django/interface/management/commands/verifyuser.py
+++ service/django/interface/management/commands/verifyuser.py
@@ -6,47 +6,101 @@ from optparse import make_option
 import os
 
 class Command(BaseCommand):
-	option_list = BaseCommand.option_list + (
-		make_option('--admin','-a',dest='admin',action='store_true',default=False),
-		make_option('--disable','-d',dest='disable',action='store_true',default=False),
-		make_option('--email','-e',dest='email',action='store'),
-		make_option('--groups','-g',dest='groups',action='store'),
-		make_option('--password','-p',dest='password',action='store'),
-		make_option('--resave','-r',dest='resave',action='store_true',default=False),
-		make_option('--resave_password','-f',dest='resave_password',action='store_true',default=False),
-		make_option('--superuser','-s',dest='superuser',action='store_true',default=False),
-		make_option('--through','-t',dest='through',action='store'),
-		make_option('--username','-u',dest='username',action='store'),
-	)
-	help = 'Verifies user exists.  Sets admin/superuser status as requested.'
+    help = 'Verifies user exists.  Sets admin/superuser status as requested.'
 
-	def handle(self, **options):
-		disable = options.get('disable')
-		password = options.get('password')
-		username = options.get('username')
-		if disable and username:
-			try:
-				user = User.objects.get(username=username)
-				user.set_password(username)
-				user.is_active = False
-				user.save()
-			except User.DoesNotExist:
-				pass
-		elif password and username:
-			user, created = User.objects.get_or_create(username=username)
-			if user and (created or options.get('resave')):
-				if created or (options.get('resave') and options.get('resave_password')):
-					user.set_password(password)
-				if options.get('email'):
-					user.email = options.get('email')
-				user.is_staff = options.get('admin')
-				user.is_active = True
-				user.is_superuser = options.get('superuser')
-				user.save()
-				if options.get('groups'):
-					groups = options.get('groups').split(',')
-					for group in groups:
-						g = Group.objects.get(name=group) 
-						g.user_set.add(user)
-		else:
-			raise Exception("Missing parameter.")
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--admin',
+            action='store_true',
+            default=False,
+            dest='admin',
+            help='Give admin privileges',
+        )
+        parser.add_argument(
+            '--disable',
+            action='store_true',
+            default=False,
+            dest='disable',
+            help='Turn off user access',
+        )
+        parser.add_argument(
+            '--email',
+            action='store',
+            dest='email',
+            help='Set as user's email address',
+        )
+        parser.add_argument(
+            '--groups',
+            action='store',
+            dest='groups',
+            help='User permission groups',
+        )
+        parser.add_argument(
+            '--password',
+            action='store',
+            dest='password',
+            help='Setting for user password',
+        )
+        parser.add_argument(
+            '--resave',
+            action='store_true',
+            default=False,
+            dest='resave',
+            help='Force update of all specified parameters',
+        )
+        parser.add_argument(
+            '--resave_password',
+            action='store_true',
+            default=False,
+            dest='resave_password',
+            help='Force update user password',
+        )
+        parser.add_argument(
+            '--superuser',
+            action='store_true',
+            default=False,
+            dest='superuser',
+            help='Give superuser privileges',
+        )
+        parser.add_argument(
+            '--through',
+            action='store',
+            dest='through',
+        )
+        parser.add_argument(
+            '--username',
+            action='store',
+            dest='username',
+            help='ID of user being updated/added',
+        )
+
+    def handle(self, *args, **options):
+        disable = options.get('disable')
+        password = options.get('password')
+        username = options.get('username')
+        if disable and username:
+            try:
+                user = User.objects.get(username=username)
+                user.set_password(username)
+                user.is_active = False
+                user.save()
+            except User.DoesNotExist:
+                pass
+        elif password and username:
+            user, created = User.objects.get_or_create(username=username)
+            if user and (created or options.get('resave')):
+                if created or (options.get('resave') and options.get('resave_password')):
+                    user.set_password(password)
+                if options.get('email'):
+                    user.email = options.get('email')
+                user.is_staff = options.get('admin')
+                user.is_active = True
+                user.is_superuser = options.get('superuser')
+                user.save()
+                if options.get('groups'):
+                    groups = options.get('groups').split(',')
+                    for group in groups:
+                        g = Group.objects.get(name=group) 
+                        g.user_set.add(user)
+        else:
+            raise Exception("Missing parameter.")
