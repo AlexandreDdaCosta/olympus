427b89b57b2d9deb46880129982164700887de0e
Alex da Costa <alex_investor@yahoo.com>
2017-07-07 18:16:06 -0400

    Initial TDD setup

--- service/node/restapi/api/routes/index.js
+++ service/node/restapi/api/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res) {
+    console.log('/ index.js');
     res.json({ message: 'Olympus back-end API listening for requests via express/node.js.' });   
 });
 module.exports = router; 
--- service/node/restapi/tests/integration/index.js
+++ service/node/restapi/tests/integration/index.js
@@ -1,16 +1,23 @@
+var chai = require('chai');
 var http = require('http');
-var options = {
-  host: '127.0.0.1',
-  port: 8889,
-  path: '/api/'
-};
-http.get(options, function(res){
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (jsonstring) {
-    console.log('JSON: '+ jsonstring);
-  });
-}).on("error", function(e){
-  console.log('ERROR: ' + e.message);
+var expect = chai.expect;
+
+describe('API index', function() {
+    it('should return a 200 response code and welcome message', function() {
+        var options = {
+            host: '127.0.0.1',
+            port: 8889,
+            path: '/api/'
+        };
+        http.get(options, function(res) {
+            console.log("Got response: " + res.statusCode);
+            res.on("data", function(chunk) {
+                console.log("BODY: " + chunk);
+            });
+        }).on('error', function(e) {
+            console.log("Got error: " + e.message);
+        });
+        // expect(statusCode).to.equal(200);
+    });
 });
+
