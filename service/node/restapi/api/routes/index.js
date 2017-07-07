468a7f02e4c71145d04634f15a0fc5d32e7cad8e
Alex da Costa <alex_investor@yahoo.com>
2017-07-07 15:15:50 -0400

    Debugging log file updates

--- service/node/restapi/api/routes/index.js
+++ service/node/restapi/api/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res) {
-    res.json({ message: 'Olympus back-end API listening for requests.' });   
+    res.json({ message: 'Olympus back-end API listening for requests via express/node.js.' });   
 });
 module.exports = router; 
--- service/node/restapi/tests/integration/index.js
+++ service/node/restapi/tests/integration/index.js
@@ -1,1 +1,2 @@
 var http = require('http');
+
