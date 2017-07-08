3ce1ab99cf9bd6c6d48a0a87e79fcec496a02589
Alex da Costa <alex_investor@yahoo.com>
2017-07-07 20:58:31 -0400

    Completed basic API connection test

--- service/node/restapi/tests/integration/index.js
+++ service/node/restapi/tests/integration/index.js
@@ -1,31 +1,16 @@
 var chai = require('chai');
-var http = require('http');
 var request = require('request');
 var expect = chai.expect;
 
+var uri = 'http://127.0.0.1:8889/api/';
+var message = 'Olympus back-end API listening for requests via express/node.js.'
+
 describe('Index page of API, basic functioning', function () {
-  it('should return HTTP code 200 with pre-formatted JSON reply', function (done) {
-    var options = {
-      host: '127.0.0.1',
-      port: 8889,
-      path: '/api/'
-    };
-    http.get(options, function (response){
-      response.setEncoding('utf8');
-      var body = '';
-      response.on('data', function (chunk) {
-        console.log('DATABODY: ' + chunk);
-        body += chunk;
-      });
-      response.on('end', function () {
-        console.log('PREBODY: ' + body);
-        expect(body).to.not.equal(200);
-        console.log('POSTBODY: ' + body);
-      });
-      console.log('precode');
-      console.log(response.statusCode);
+  it('should return HTTP code 200 with JSON reply including a "message" key', function (done) {
+    request.get(uri, function (err, response, body){
       expect(response.statusCode).to.equal(200);
-      expect(response.statusCode).to.not.equal(0);
+      jsonobject = JSON.parse(response.body);
+      expect(jsonobject['message']).to.equal(message);
       done();
     });
   });
