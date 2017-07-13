6ddab1eb41f6b1aafcbe5c05bc59cd4ffd607252
Alex da Costa <alex_investor@yahoo.com>
2017-07-12 23:19:24 -0400

    8889 and 4443 ports working for backend access

--- service/node/restapi/tests/integration/index.js
+++ service/node/restapi/tests/integration/index.js
@@ -1,9 +1,10 @@
 var chai = require('chai');
 var expect = chai.expect;
+var fs = require('fs'); 
+var https = require('https'); 
 var request = require('request');
 
 var url = 'http://zeus:8889/api/';
-var secureurl = 'https://zeus:4443/api/';
 var message = 'Olympus back-end API listening for requests via express/node.js.'
 
 describe('Connection to HTTP index page of API', function () {
@@ -18,14 +19,23 @@ describe('Connection to HTTP index page of API', function () {
   });
 });
 
+var options = { 
+    hostname: 'zeus', 
+    port: 4443, 
+    path: '/api', 
+    method: 'get',
+    ca: fs.readFileSync('/etc/ssl/certs/ca-crt.pem.pem')
+}; 
 describe('Connection to HTTPS index page of API', function () {
   it('Should return HTTP code 200 with JSON reply including a "message" key.', function (done) {
-    request.get(secureurl, function (err, response, body){
-      if (err) return done(err);
+    var request = https.request(options, function(response) { 
       expect(response.statusCode).to.equal(200);
-      jsonobject = JSON.parse(response.body);
-      expect(jsonobject['message']).to.equal(message);
+      response.on('data', function(data) { 
+        jsonobject = JSON.parse(data);
+        expect(jsonobject['message']).to.equal(message);
+      }); 
       done();
     });
-  });
-});
+    request.end();
+  }); 
+}); 
