c7ce3f9416541d1761a1b2a62092c40f53457580
Alex da Costa <alex_investor@yahoo.com>
2017-07-26 01:21:44 -0400

    Ploutos initial testfile, mongodb

--- project/ploutos/test/mongo.py
+++ project/ploutos/test/mongo.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-import unittest
+import pymongo, unittest
 
 import olympus.testing as testing
 
-URL = 0
+CAFILE = '/usr/local/share/ca-certificates/ca-crt-supervisor.pem.crt'
+CERTFILE = '/etc/ssl/localcerts/client-crt.pem'
+KEYFILE = '/etc/ssl/localcerts/client-key.pem'
+URL = 'mongodb://zeus:27017/olympus?ssl=true';
 
 class TestMongo(testing.Test):
 
@@ -12,7 +15,12 @@ class TestMongo(testing.Test):
         pass
 
     def test_connect(self):
-        pass
+        client = pymongo.MongoClient(URL,ssl=True,ssl_ca_certs=CAFILE,ssl_certfile=CERTFILE,ssl_keyfile=KEYFILE)
+        test_collection = client.olympus.pymongo_test
+        newdoc_obj = test_collection.insert_one({'x': 1})
+        print(test_collection.find({'x': 1}))
+        test_collection.drop()
+
 
 if __name__ == '__main__':
 	unittest.main()
