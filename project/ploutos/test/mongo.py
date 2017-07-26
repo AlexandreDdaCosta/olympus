5851f6152c10f9f6cb0c27c7b15aa5c555f31165
Alex da Costa <alex_investor@yahoo.com>
2017-07-26 00:26:32 -0400

    Basic mongo tests for Project Ploutos

---
+++ project/ploutos/test/mongo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import olympus.testing as testing
+
+URL = 0
+
+class TestMongo(testing.Test):
+
+    def setUp(self):
+        pass
+
+    def test_connect(self):
+        pass
+
+if __name__ == '__main__':
+	unittest.main()
