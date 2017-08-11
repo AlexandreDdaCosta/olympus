2bd692842fd2580ed49d8ad2d593e4b499e15362
Alex da Costa <alex_investor@yahoo.com>
2017-08-11 16:11:20 -0400

    Graceful update to init script

--- project/ploutos/scripts/init.py
+++ project/ploutos/scripts/init.py
@@ -4,10 +4,10 @@ from olympus.projects.ploutos.data.options import InitOptions
 from olympus.projects.ploutos.data.symbols import InitSymbols
 
 print("Begin symbol import.")
-process = InitSymbols()
+process = InitSymbols(graceful=True)
 process.populate_collections()
 print("\nEnded symbol import.")
 print("Begin options import.")
-process = InitOptions()
+process = InitOptions(graceful=True)
 process.populate_collections()
 print("\nEnded options import.")
