999123085acac7ea7f65d7e1c7da45dc4ebbdaf7
Alex da Costa <alex_investor@yahoo.com>
2017-08-11 00:49:26 -0400

    Buggy constant

--- service/salt/fs/projects/ploutos/files/lib/conf.py
+++ service/salt/fs/projects/ploutos/files/lib/conf.py
@@ -4,5 +4,5 @@ USER = 'ploutos'
 USER_HOME = '/home/' + USER + '/'
 
 DOWNLOAD_DIR = USER_HOME + 'Downloads/'
-LOCKFILE_DIR = '/var/run/olympus/projects/'+USER+'ploutos/'
+LOCKFILE_DIR = '/var/run/olympus/projects/'+USER+'/'
 WORKING_DIR = USER_HOME
