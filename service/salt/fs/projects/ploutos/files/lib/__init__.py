4c08256d73a7b2662ae514e55fdf29caff293316
Alex da Costa <alex_investor@yahoo.com>
2017-08-20 16:43:52 -0400

    Price retrieval plus test

--- service/salt/fs/projects/ploutos/files/lib/__init__.py
+++ service/salt/fs/projects/ploutos/files/lib/__init__.py
@@ -6,3 +6,8 @@ USER_HOME = '/home/' + USER + '/'
 DOWNLOAD_DIR = USER_HOME + 'Downloads/'
 LOCKFILE_DIR = '/var/run/olympus/projects/'+USER+'/'
 WORKING_DIR = USER_HOME
+
+TEST_SYMBOL_ONE = 'goog'
+TEST_SYMBOL_ONE_EXCHANGE = 'nasdaq'
+TEST_SYMBOL_TWO = 'gg'
+TEST_SYMBOL_TWO_EXCHANGE = 'nyse'
