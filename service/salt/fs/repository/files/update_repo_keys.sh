b454bc0d7eb408c9e4816275aa1c0fddd621c91a
Alex da Costa <alex_investor@yahoo.com>
2018-09-04 23:55:16 -0400

    Script to update expired gnupg keys

--- service/salt/fs/repository/files/update_repo_keys.sh
+++ service/salt/fs/repository/files/update_repo_keys.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-apt-key list | grep expired
-if [ $? -eq 0 ]
-then
-  echo "OK"
-  exit 1
-else
-  echo "Key verification failure"
-  exit 1
-fi
+expired_list="$(apt-key list | grep expired)"
+while read -r line
+do
+  IFS=' '
+  read -ra expired_key <<< "$line"
+  IFS=/
+  read -ra key_id <<< "${expired_key[1]}"
+  if [ ! -z "$key_id" ]
+  then
+    echo "Updating ${key_id[1]}"
+    apt-key adv --keyserver keys.gnupg.net --recv-keys "${key_id[1]}"
+  fi
+done <<< "$expired_list"
+exit 0
