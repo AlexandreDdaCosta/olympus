f451c7879a383b4143103cf0fa4eef36e4895f68
Alex da Costa <alex_investor@yahoo.com>
2018-09-10 02:12:34 -0400

    Script development

--- service/salt/util/package_version_repo_updater.pl
+++ service/salt/util/package_version_repo_updater.pl
@@ -8,28 +8,87 @@ use strict;
 use Data::Dumper;
 use Cwd qw{abs_path getcwd};
 
-use vars qw{@GIT_FILES $PACKAGE_VERSIONS};
-my @GIT_FILES= ('../../salt/pillar/services.sls','../../salt/pillar/distribution.sls');
+my $TEST = $ARGV[0] || 0;
+if ($TEST) { print "TEST MODE\n" }
 
-print abs_path($0)."\n";
+use vars qw{$PACKAGE_VERSIONS};
+my $GIT_FILE_DIR= '../../salt/pillar/';
+my @GIT_FILES= ('services.sls','distribution.sls');
+my $TEST_SUFFIX = q{.TEST};
+
 my $abs_dir = abs_path($0);
 $abs_dir =~ s/^(.*)(\/)(.*?)$/$1/;
 chdir $abs_dir;
-foreach my $filepath (@GIT_FILES)
+foreach my $filename (@GIT_FILES)
 {
-    my $filename = $filepath;
-    $filename =~ s/^(.*)(\/)(.*?)$/$3/;
+    if ($TEST)
+    {
+        $filename .= $TEST_SUFFIX;
+    }
     print "Finding $filename\n";
-    (-e $filepath) || die "Can't locate $filename; is executable in a git repo?";
+    my $filepath = $GIT_FILE_DIR . $filename;
+    (-e $filepath) || die "Can't locate $filename. Pillar file must be in a git repo, along with this script.";
     $PACKAGE_VERSIONS->{$filename} = {};
+    my $installer = undef;
+    my $package = undef;
 	open(GITFILE,"$filepath") || die "Can't open $filepath";
     while (<GITFILE>)
     {
-        if ($_ =~ /\-packages\:$/)
+        chop;
+        if ($_ =~ /^[^\s]*\-packages\:$/)
         {
-            print $_."\n";
+            print "Package stanza $_\n";
+            # pip3, npm, gem, <none>
+            if ($_ =~ /\-pip3\-/)
+            {
+                $installer = 'pip3'
+            }
+            elsif ($_ =~ /\-npm\-/)
+            {
+                $installer = 'npm'
+            }
+            elsif ($_ =~ /\-gem\-/)
+            {
+                $installer = 'gem'
+            }
+            else
+            {
+                $installer = 'apt'
+            }
+            $PACKAGE_VERSIONS->{$filename}->{$installer} = {};
+            next;
+        }
+        elsif ($_ =~ /^\s*?$/)
+        {
+            # end of stanza
+            $installer = undef;
+            $package = undef;
+            next;
+        }
+        if (defined $installer)
+        {
+            if (defined $package)
+            {
+                if ($_ =~ /^\s+version\:/)
+                {
+                    my $version = $_;
+                    $version =~ s/^(\s+version\:\s*)([^\s]*)(\s*?)$/$2/;
+                    $PACKAGE_VERSIONS->{$filename}->{$installer}->{$package} = $version;
+                }
+            }
+            elsif ($_ =~ /^  [^\s]+/)
+            {
+                # Package name
+                $package = $_;
+                $package =~ s/\:$//;
+                $package =~ s/\s//g;
+                print qq{package $package\n};
+                next;
+            }
         }
     }
 	close(GITFILE);
 }
+print Dumper $PACKAGE_VERSIONS;
 
+# Get apt, pip3, npm, gem installed data
