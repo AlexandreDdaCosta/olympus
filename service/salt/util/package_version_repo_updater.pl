a6278488d5a5001b5b970f9a0239c789151fc8e8
Alex da Costa <alex_investor@yahoo.com>
2018-09-11 00:28:29 -0400

    Latest revs to script

--- service/salt/util/package_version_repo_updater.pl
+++ service/salt/util/package_version_repo_updater.pl
@@ -8,23 +8,16 @@ use strict;
 use Data::Dumper;
 use Cwd qw{abs_path getcwd};
 
-my $TEST = $ARGV[0] || 0;
-if ($TEST) { print "TEST MODE\n" }
-
 use vars qw{$PACKAGE_VERSIONS};
-my $GIT_FILE_DIR= '../../salt/pillar/';
+my $GIT_FILE_DIR= q{../../salt/pillar/};
 my @GIT_FILES= ('services.sls','distribution.sls');
-my $TEST_SUFFIX = q{.TEST};
+my $outfile_prefix = q{/tmp/package_version_repo_updater.}.time().q{.};
 
 my $abs_dir = abs_path($0);
 $abs_dir =~ s/^(.*)(\/)(.*?)$/$1/;
 chdir $abs_dir;
 foreach my $filename (@GIT_FILES)
 {
-    if ($TEST)
-    {
-        $filename .= $TEST_SUFFIX;
-    }
     print "Finding $filename\n";
     my $filepath = $GIT_FILE_DIR . $filename;
     (-e $filepath) || die "Can't locate $filename. Pillar file must be in a git repo, along with this script.";
@@ -35,9 +28,10 @@ foreach my $filename (@GIT_FILES)
     while (<GITFILE>)
     {
         chop;
-        if ($_ =~ /^[^\s]*\-packages\:$/)
+        #print $_.qq{\n};
+        if ($_ =~ /^[^\s]*packages\:$/)
         {
-            print "Package stanza $_\n";
+            #print "Package stanza $_\n";
             # pip3, npm, gem, <none>
             if ($_ =~ /\-pip3\-/)
             {
@@ -55,30 +49,40 @@ foreach my $filename (@GIT_FILES)
             {
                 $installer = 'apt'
             }
-            $PACKAGE_VERSIONS->{$filename}->{$installer} = {};
+            if (! exists $PACKAGE_VERSIONS->{$filename}->{$installer})
+            {
+                $PACKAGE_VERSIONS->{$filename}->{$installer} = {};
+            }
             next;
         }
         elsif ($_ =~ /^\s*?$/)
         {
-            # end of stanza
+            # divider or end of stanza
             $installer = undef;
             $package = undef;
             next;
         }
         if (defined $installer)
         {
-            if (defined $package)
+            if (defined $package and $_ !~ /^  [^\s]+/)
             {
                 if ($_ =~ /^\s+version\:/)
                 {
                     my $version = $_;
-                    $version =~ s/^(\s+version\:\s*)([^\s]*)(\s*?)$/$2/;
+                    if ($installer ne 'pip3')
+                    {
+                        $version =~ s/^(\s+version\:\s*)([^\s]*)(\s*?)$/$2/;
+                    }
+                    else
+                    {
+                        $version =~ s/^(\s+version\:\s*\=\=\s*)([^\s]*)(\s*?)$/$2/;
+                    }
                     $PACKAGE_VERSIONS->{$filename}->{$installer}->{$package} = $version;
                 }
             }
             elsif ($_ =~ /^  [^\s]+/)
             {
-                # Package name
+                # package name
                 $package = $_;
                 $package =~ s/\:$//;
                 $package =~ s/\s//g;
@@ -92,3 +96,17 @@ foreach my $filename (@GIT_FILES)
 print Dumper $PACKAGE_VERSIONS;
 
 # Get apt, pip3, npm, gem installed data
+
+# Output
+
+foreach my $filename (@GIT_FILES)
+{
+    # create output file
+
+    my $outfile = $outfile_prefix . $filename;
+    open OUTFILE, ">$outfile" or die "Failed to create output file $outfile";
+    close OUTFILE;
+    print qq{Outfile for $filename created in $outfile\n};
+    system(qq{ls -la $outfile});
+}
+exit 0
