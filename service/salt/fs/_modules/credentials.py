a1233497c3128c7b52cc91900fa9f9a48ba91b28
Alex da Costa <alex_investor@yahoo.com>
2018-09-14 14:25:52 -0400

    database()

--- service/salt/fs/_modules/credentials.py
+++ service/salt/fs/_modules/credentials.py
@@ -7,4 +7,7 @@ Tools for managing cross-server credentials
 import json
 
 def database():
-    return True
+    credential_file = __salt__['pillar.get']('db_credential_file', None)
+    exclude_server = __salt__['pillar.get']('db_credential_exclude_server_type', None)
+    server = __grains__['server']
+    return credential_file + ' ' + exclude_server + ' ' + server
