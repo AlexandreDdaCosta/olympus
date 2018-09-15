aa81a4b3a5645f57af6ec4d6bdd56e33be972f39
Alex da Costa <alex_investor@yahoo.com>
2018-09-15 01:33:15 -0400

    codetest

--- service/salt/fs/_modules/credentials.py
+++ service/salt/fs/_modules/credentials.py
@@ -31,7 +31,7 @@ def database():
                 cmd = "sudo -u postgres psql -tAc \"SELECT rolname FROM pg_roles WHERE rolname='" + frontend_user + "'\""
                 p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
                 output = p.communicate()
-                last = output.pop()
+                last = output[-1]
                 return last
                 # Update frontend user password
                 cmd = "sudo -u postgres psql -c \"ALTER USER " + frontend_user  + " ENCRYPTED PASSWORD '" + passphrase  + "';\""
