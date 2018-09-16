b19015b690d9928a79e7b473195be176b4675ddb
Alex da Costa <alex_investor@yahoo.com>
2018-09-15 23:39:22 -0400

    ct

--- service/salt/fs/_modules/credentials.py
+++ service/salt/fs/_modules/credentials.py
@@ -42,7 +42,7 @@ def database():
             frontend_credential_file = '/srv/www/django/interface/settings_local.py'
             if os.path.isfile(frontend_credential_file):
                 # If frontend configuration exists, update password
-                cmd = "perl -e 'open my $in, \"" + frontend_credential_file  + "\" or die \"$!\"; $/ = undef; my $all = <$in>; close $in; $all =~ s/(\\'PASSWORD\\'\\:\s*\\')(.*?)(\\')/$1" + passphrase + "$3/; open my $out, \">$file\" or die \"$!\"; print $out $all; close $out;'"
+                cmd = "perl -i -pe 's/('\''PASSWORD'\''\:\s+'\'')(.*?)('\'')/$1" + passphrase + "$3/g' /tmp/test"
                 p = subprocess.check_call(cmd,shell=True)
                 # If frontend web service is running, restart
                 cmd = "ps -A | grep uwsgi | wc -l"
