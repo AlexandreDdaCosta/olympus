49246d26eff6d53cbb75b66b2d17d4699885b76a
Alex da Costa <alex_investor@yahoo.com>
2018-09-17 13:52:37 -0400

    Backup execution module. Initial USB stick backup function.

---
+++ service/salt/fs/_modules/backup.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+'''
+Manage data backup and restoration
+'''
+
+import os, subprocess
+
+def usb_backup_olympus():
+    # Verify presence of olympus USB
+    cmd = "blkid"
+    p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+    output = p.communicate()
+    return output
+
+def foo_shared_database():
+    frontend_user = __salt__['pillar.get']('frontend-user')
+    passphrase = __salt__['data.get']('frontend_db_key')
+    server = __grains__['server']
+    services = None
+    if (passphrase is not None and server is not None):
+        key = server + ':services'
+        services = __salt__['pillar.get'](key)
+        delete_minion_data = False
+        if 'backend' in services:
+            # Is database running?
+            cmd = "ps -A | grep postgres | wc -l"
+            p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+            backend_processes = p.communicate()[0].strip("\n")
+            if int(backend_processes) > 0:
+                # Does frontend user exist?
+                cmd = "sudo -u postgres psql -tAc \"SELECT rolname FROM pg_roles WHERE rolname='" + frontend_user + "'\""
+                p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+                output = p.communicate()
+                rolname = output[0].split("\n")[-2]
+                if rolname == frontend_user:
+                    # Update frontend user password
+                    cmd = "sudo -u postgres psql -c \"ALTER USER " + frontend_user  + " ENCRYPTED PASSWORD '" + passphrase  + "';\""
+                    p = subprocess.check_call(cmd,shell=True)
+                    delete_minion_data = True
+        if 'frontend' in services:
+            delete_minion_data = False
+            frontend_credential_file = '/srv/www/django/interface/settings_local.py'
+            if os.path.isfile(frontend_credential_file):
+                # If frontend configuration exists, update password
+                cmd = "perl -i -pe 's/('\\''PASSWORD'\\''\\:\\s+'\\'')(.*?)('\\'')/$1" + passphrase + "$3/g' " + frontend_credential_file
+                p = subprocess.check_call(cmd,shell=True)
+                # If dev frontend web service is running, restart
+                cmd = "ps -A | grep runserver | wc -l"
+                p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+                frontend_dev_processes = p.communicate()[0].strip("\n")
+                if int(frontend_dev_processes) > 0:
+                    cmd = "/usr/local/bin/killserver.sh"
+                    p = subprocess.check_call(cmd,shell=True)
+                    cmd = "/usr/local/bin/startserver.py"
+                    p = subprocess.check_call(cmd,shell=True)
+                else:
+                    # If frontend web service is running, restart
+                    cmd = "ps -A | grep uwsgi | wc -l"
+                    p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+                    frontend_processes = p.communicate()[0].strip("\n")
+                    if int(frontend_processes) > 0:
+                        cmd = "service uwsgi restart"
+                        p = subprocess.check_call(cmd,shell=True)
+        if delete_minion_data is True:
+            __salt__['data.pop']('frontend_db_key')
+    return True
