1f17575c73d5213c440702d965132712dcf9aba7
Alex da Costa <alex_investor@yahoo.com>
2018-09-20 16:24:25 -0400

    Django2 incompatibility with ForeignKey in models

--- service/django/interface/apps/blog/models.py
+++ service/django/interface/apps/blog/models.py
@@ -13,34 +13,34 @@ class Graphic(models.Model):
 
 class Topic(models.Model):
     description = models.CharField(max_length=255)
-    graphic = models.ForeignKey(Graphic)
+    graphic = models.ForeignKey(Graphic,on_delete=models.CASCADE)
     name = models.CharField(db_index=True,max_length=30,unique=True,default="/")
 
     def __unicode__(self):
         return '%s' % (self.name)
 
 class Article(models.Model):
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     body = models.TextField(null=True)
     create_date = models.DateTimeField(_('Created'), auto_now_add=True)
     featured = models.BooleanField(blank=True,default=False)
-    featured_graphic = models.ForeignKey(Graphic,null=True)
+    featured_graphic = models.ForeignKey(Graphic,on_delete=models.CASCADE,null=True)
     graphics = models.ManyToManyField(Graphic,related_name='article_graphics')
     published = models.BooleanField(blank=True,default=False)
     summary = models.TextField(null=True)
     title = models.CharField(db_index=True,max_length=255)
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
 
     def __unicode__(self):
         return '%s' % (self.title)
 	
 class Article_Revision(models.Model):
     archive_date = models.DateTimeField(_('Archived'),auto_now_add=True)
-    article = models.ForeignKey(Article)
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
     body = models.TextField(null=True)
     summary = models.TextField(null=True)
     title = models.CharField(db_index=True,max_length=255)
-    topic = models.ForeignKey(Topic,null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE,null=True)
 
     def __unicode__(self):
         return '%s' % (self.title)
@@ -53,8 +53,8 @@ class Keyword(models.Model):
         return '%s' % (self.name)
 
 class Keyword_Article(models.Model):
-    article = models.ForeignKey(Article)
-    keyword = models.ForeignKey(Keyword)
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    keyword = models.ForeignKey(Keyword,on_delete=models.CASCADE)
     occurrences = models.PositiveIntegerField()
 
     def __unicode__(self):
