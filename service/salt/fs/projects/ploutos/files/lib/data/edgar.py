ba6fd26cd5ace2aeaf2da8c0454834d7fc1f9eda
Alex da Costa <alex_investor@yahoo.com>
2018-09-25 02:05:09 -0400

    First added collection for form4

--- service/salt/fs/projects/ploutos/files/lib/data/edgar.py
+++ service/salt/fs/projects/ploutos/files/lib/data/edgar.py
@@ -1,4 +1,4 @@
-import datetime, fcntl
+import datetime, fcntl, json, re
 import edgar as form4_index_downloader
 
 import olympus.projects.ploutos.data as data
@@ -6,7 +6,7 @@ import olympus.projects.ploutos.data as data
 from olympus.projects.ploutos import *
 from olympus.projects.ploutos.data import *
 
-FORM4_INDEX_COLLECTIONS_PREFIX = 'form4_'
+FORM4_INDEX_COLLECTIONS_PREFIX = 'form4_index_'
 FORM4_INDEX_SUFFIX = '_form4' + INDEX_SUFFIX
 QUARTERLY_FIRST_YEAR = 2004
 QUARTERLY_ORIGINAL_YEAR = 1993
@@ -40,15 +40,14 @@ class InitForm4Indices(data.Connection):
         if self._initialized() != socket.gethostname():
             raise Exception('Initialization record check failed; cannot record start of initialization.')
 
-		# Check for existing completed years/quarters
-        # The edgar module retrieves based on a starting date. Our check is to see 
-        # which data sets have already been retrieved and indexed.
+        # The edgar module retrieves based on a starting year. 
+        # Check is to see which data sets have already been retrieved and indexed.
 
         # Index/create collection
         existing_collections = self.db.collection_names()
         start_year = datetime.datetime.now().year+1
         for year in QUARTERLY_YEAR_LIST:
-            collection_name = FORM4_INDEX_COLLECTIONS_PREFIX+'quarterlies_'+str(year)
+            collection_name = FORM4_INDEX_COLLECTIONS_PREFIX+str(year)
             if collection_name not in existing_collections:
                 start_year = year
                 break
@@ -57,17 +56,42 @@ class InitForm4Indices(data.Connection):
 
 		# Download
 
+        download_directory = '/tmp/form4_indices_2018-09-25_03_20_48.208421/'
         #download_directory = '/tmp/form4_indices_'+str(datetime.datetime.utcnow()).replace(" ", "_").replace(":", "_")
         #if not os.path.isdir(download_directory):
         #    os.mkdir(download_directory)
         #form4_index_downloader.download_index(download_directory,start_year)
 
-        # Clean up received data
+        # Create and populate Form 4 index collections, by year
 
-        
-        # Create collection
-		
-        #collection.create_index([('quarter', pymongo.ASCENDING)], name='quarter'+FORM4_INDEX_SUFFIX, unique=False)
+        collection_range = range(start_year,datetime.datetime.now().year+1)
+        for year in collection_range:
+            json_data = '['
+            for quarter in (1, 2, 3, 4):
+                filename = download_directory + str(year) + '-QTR' + str(quarter) + '.tsv'
+                if os.path.isfile(filename):
+                    with open(filename) as f:
+                        for line in f:
+                            pieces = line.rstrip().split('|')
+                            if pieces[2][:1] != '4':
+                                continue
+                            row = {}
+                            row['cik'] = pieces[0]
+                            row['file'] = pieces[4]
+                            # "edgar/data/1000015/0001047469-04-001808.txt"
+                            row['file'] = re.sub(r'^.*\/(.*)\.txt',r'\g<1>',pieces[4])
+                            jsonstring = json.dumps(row)
+                            json_data += '\n'+jsonstring+','
+            if len(json_data) > 1:
+                json_data = json_data[:-1]
+            json_data += '\n]'
+            out_data = json.loads(json_data)
+            collection_name = FORM4_INDEX_COLLECTIONS_PREFIX+str(year)
+            collection = self.db[FORM4_INDEX_COLLECTIONS_PREFIX+str(year)]
+            collection.insert_many(out_data)
+            #collection.create_index("Symbol")
+            #collection.create_index([('quarter', pymongo.ASCENDING)], name='quarter'+FORM4_INDEX_SUFFIX, unique=False)
+            raise Exception('ALEX')
 		
         # Unlock process
 		
