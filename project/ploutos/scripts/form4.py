b52d6cf4742439c64cf7b37fd549eb74b2a93aff
Alex da Costa <alex_investor@yahoo.com>
2018-09-29 18:19:25 -0400

    First working version

--- project/ploutos/scripts/form4.py
+++ project/ploutos/scripts/form4.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import time
+import sys, time
 
 from argparse import ArgumentParser
 
 from olympus.projects.ploutos.data.edgar import Form4
 
-parser = ArgumentParser()
+parser = ArgumentParser(sys.argv)
 parser.add_argument("-a","--action",choices=['get_indexed_forms'],help="Action for this script to perform",required=True)
 parser.add_argument("-v","--verbose",action="store_true",help="Chatty output")
-args, command_string = parser.parse_args()
+args = parser.parse_args()
 
 if args.verbose is True:
     start = time.time()
