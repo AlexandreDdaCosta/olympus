bed260a2c471b8246f07d0025402c442998b2066
Alex da Costa <alex_investor@yahoo.com>
2019-02-13 23:14:33 -0500

    Sart of livermore library

---
+++ apps/livermore/lib/test/market_key.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+
+import olympus.testing as testing
+
+class TestSomething(testing.Test):
+
+    def setUp(self):
+        pass
+
+    def test_foo(self):
+        pass
+
+if __name__ == '__main__':
+	unittest.main()
