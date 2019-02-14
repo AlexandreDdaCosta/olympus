436ed16df35806659e1ad80b1d019d68cc929337
Alex da Costa <alex_investor@yahoo.com>
2019-02-13 23:35:44 -0500

    Keyword bug

--- apps/livermore/lib/__init__.py
+++ apps/livermore/lib/__init__.py
@@ -8,7 +8,7 @@ CONTINUE = 3 # An integer that defines the minimum point move required to recogn
 REVERSAL = 6 # An integer that defines the minimum point move required to recognize price reversal
 
 # These functions continuation and reversal levels for multiple equities
-def KEY_CONTINUE(securities=2,continue=CONTINUE):
-    return securities * continue
+def KEY_CONTINUE(securities=2,continuation=CONTINUE):
+    return securities * continuation
 def KEY_REVERSAL(securities=2,reversal=REVERSAL):
     return securities * reversal
