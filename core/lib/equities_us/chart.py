51f587a06a6021bd020a4335dfe5c61dafb7c207
Alex da Costa <alex_investor@yahoo.com>
2019-06-15 20:04:45 -0400

    Reformatting charting and dates

---
+++ core/lib/equities_us/chart.py
@@ -0,0 +1,20 @@
+import json
+
+class Date(object):
+
+    def __init__(self,date,open,close,high,low,adjusted_close):
+        self.date = date
+        self.open = open
+        self.close = close
+        self.high = high
+        self.low = low
+        self.adjusted_close = adjusted_close
+
+    def adjusted_high(self):
+        pass
+
+    def adjusted_high(self):
+        pass
+
+    def adjusted_open(self):
+        pass
