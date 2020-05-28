467244f2913d73e9984daff1a7e0249374bf9b6d
Alex da Costa <alex_investor@yahoo.com>
2020-05-28 00:16:57 -0400

    Debugging execution modules

--- service/salt/fs/_modules/olympus.py
+++ service/salt/fs/_modules/olympus.py
@@ -5,4 +5,8 @@ Manage data backup and restoration
 '''
 
 def ping():
+    frontend_user = __salt__['pillar.get']('frontend-user')
+    passphrase = __salt__['data.get']('frontend_db_key')
+    server = __grains__['server']
+    services = None
     return True
