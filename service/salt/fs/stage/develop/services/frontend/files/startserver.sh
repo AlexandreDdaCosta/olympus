62ad69d36b89bba1d3aaeb555c695247d70dcfcc
Alex da Costa <alex_investor@yahoo.com>
2020-05-30 16:12:01 -0400

    Debugging test server

--- service/salt/fs/stage/develop/services/frontend/files/startserver.sh
+++ service/salt/fs/stage/develop/services/frontend/files/startserver.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
 sudo -u uwsgi /usr/bin/python3 /srv/www/django/manage.py runserver 1>>/var/log/devserver.log 2>>/var/log/devserver.log &
-if [ $? -eq 0 ]
-then
-  pkill -f runserver
-  if [ $? -eq 0 ]
-  then
-    echo "Dev server stopped"
-	exit 0
-  else
-    echo "Failure when stopping dev server"
-	exit 1
-  fi
-else
-  echo "Dev server not running"
-  exit 0
-fi
