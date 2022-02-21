502c1d3a24c9b4971db3f5d72767cc7d5c6d9a80
Alex da Costa <alex_investor@yahoo.com>
2022-02-20 23:53:11 -0500

    Undoing of previous change; will debug manually

--- service/django/interface/urls.py
+++ service/django/interface/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
-#    url(r'^blog/', include('interface.apps.blog.urls')),
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/welcome/')),
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('interface.apps.blog.urls')),
     url(r'^coding/', include('interface.apps.coding.urls')),
     url(r'^login/', auth_views.LoginView.as_view()),
     url(r'^logout/', auth_views.LogoutView.as_view()),
