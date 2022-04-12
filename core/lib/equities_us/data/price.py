bab03446784f0e4cafd091b0f65ba56d87e618d7
Alex da Costa <alex_investor@yahoo.com>
2022-04-12 00:17:36 -0400

    Delete real-time quotes; feature deprecated by alphadvantage

--- core/lib/equities_us/data/price.py
+++ core/lib/equities_us/data/price.py
@@ -148,34 +148,3 @@ class Quote(data.Connection):
         latest['symbol'] = symbol
         latest['volume'] = quote['06. volume']
         return latest
-
-    def real_time(self,symbols,**kwargs):
-        # "symbols" can be a list or a string
-        if isinstance(symbols,list):
-            url_symbols = ",".join(symbols)
-            url = ALPHAVANTAGE_URL + '&function=BATCH_STOCK_QUOTES&outputsize=full&symbols=' + url_symbols
-        elif isinstance(symbols,str):
-            url = ALPHAVANTAGE_URL + '&function=BATCH_STOCK_QUOTES&outputsize=full&symbols=' + symbols
-        else:
-            raise Exception("Variable 'symbols' must be a list or string.")
-        request = urllib.request.urlopen(url)
-        reply = json.loads(re.sub(r'^\s*?\/\/\s*',r'',request.read().decode("utf-8")))
-        if 'Stock Quotes' not in reply:
-            raise Exception("Reply format failure: "+str(reply))
-        real_time_quotes = {}
-        if isinstance(symbols,list):
-            for quote in reply['Stock Quotes']:
-                real_time_quote = {}
-                real_time_quote['date'] = re.sub(r'\s+.*$',r'',quote['4. timestamp'])
-                real_time_quote['price'] = quote['2. price']
-                real_time_quote['symbol'] = quote['1. symbol']
-                real_time_quote['timestamp'] = quote['4. timestamp']
-                real_time_quote['volume'] = quote['3. volume']
-                real_time_quotes[quote['1. symbol']] = real_time_quote
-        else:
-            real_time_quotes['date'] = re.sub(r'\s+.*$',r'',reply['Stock Quotes'][0]['4. timestamp'])
-            real_time_quotes['price'] = reply['Stock Quotes'][0]['2. price']
-            real_time_quotes['symbol'] = reply['Stock Quotes'][0]['1. symbol']
-            real_time_quotes['timestamp'] = reply['Stock Quotes'][0]['4. timestamp']
-            real_time_quotes['volume'] = reply['Stock Quotes'][0]['3. volume']
-        return real_time_quotes
--- core/lib/equities_us/data/test/price.py
+++ core/lib/equities_us/data/test/price.py
@@ -20,20 +20,5 @@ class TestPrice(testing.Test):
         quote = self.quote.intraday(TEST_SYMBOL_ONE)
         print(json.dumps(quote,indent=4,sort_keys=True))
 
-    def test_real_time(self):
-        quote = self.quote.real_time(TEST_SYMBOL_ONE)
-        print(json.dumps(quote,indent=4,sort_keys=True))
-        self.assertEqual(quote['symbol'].upper(),TEST_SYMBOL_ONE.upper(),'Returned symbol should match requested.')
-        self.assertTrue(quote['volume'].isdigit,'Volume is not indicated by an integer, single quote')
-        self.assertIsFloat(quote['price'],'Price in single quote has an invalid format')
-        quotes = self.quote.real_time([TEST_SYMBOL_ONE,TEST_SYMBOL_TWO])
-        print(json.dumps(quotes,indent=4,sort_keys=True))
-        self.assertEqual(quotes[TEST_SYMBOL_ONE.upper()]['symbol'].upper(),TEST_SYMBOL_ONE.upper(),'First returned symbol should match requested.')
-        self.assertTrue(quotes[TEST_SYMBOL_ONE.upper()]['volume'].isdigit,'Volume is not indicated by an integer, first quote of two')
-        self.assertIsFloat(quotes[TEST_SYMBOL_ONE.upper()]['price'],'First security price has an invalid format')
-        self.assertEqual(quotes[TEST_SYMBOL_TWO.upper()]['symbol'].upper(),TEST_SYMBOL_TWO.upper(),'Second returned symbol should match requested.')
-        self.assertTrue(quotes[TEST_SYMBOL_TWO.upper()]['volume'].isdigit,'Volume is not indicated by an integer, second quote of two')
-        self.assertIsFloat(quotes[TEST_SYMBOL_TWO.upper()]['price'],'Second security price has an invalid format')
-
 if __name__ == '__main__':
 	unittest.main()
