ea449ec6602628666e25aead566545b6a9f17b34
Alex da Costa <alex_investor@yahoo.com>
2022-04-11 23:20:14 -0400

    Removed "^" and "$" in URL specification per update to django.urls

--- service/django/interface/urls.py
+++ service/django/interface/urls.py
@@ -4,12 +4,12 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path(r'^$', RedirectView.as_view(url='/welcome/')),
-    path(r'^admin/', admin.site.urls),
-    path(r'^blog/', include('interface.apps.blog.urls')),
-    path(r'^coding/', include('interface.apps.coding.urls')),
-    path(r'^login/', auth_views.LoginView.as_view()),
-    path(r'^logout/', auth_views.LogoutView.as_view()),
-    path(r'^photography/', include('interface.apps.photography.urls')),
-    path(r'^welcome/', include('interface.apps.welcome.urls')),
+    path(r'', RedirectView.as_view(url='/welcome/')),
+    path(r'admin/', admin.site.urls),
+    path(r'blog/', include('interface.apps.blog.urls')),
+    path(r'coding/', include('interface.apps.coding.urls')),
+    path(r'login/', auth_views.LoginView.as_view()),
+    path(r'logout/', auth_views.LogoutView.as_view()),
+    path(r'photography/', include('interface.apps.photography.urls')),
+    path(r'welcome/', include('interface.apps.welcome.urls')),
 ]
