9143e286e7ad49126f6336a5fe78fe765a68afde
Alex da Costa <alex_investor@yahoo.com>
2022-04-24 18:14:42 -0400

    Jinja2 newline testing Switched node.js testing to jest

--- service/node/restapi/tests/integration/index.js
+++ service/node/restapi/tests/integration/index.js
@@ -7,8 +7,8 @@ const request = require('request');
 const url = 'http://zeus:8889/api/';
 const message = 'Olympus back-end API listening for requests via express/node.js.'
 
-describe('Connection to HTTP index page of API, node port', () {
-  test('Should return HTTP code 200 with JSON reply including a "message" key.', () {
+describe('Connection to HTTP index page of API, node port', () => {
+  test('Should return HTTP code 200 with JSON reply including a "message" key.', () => {
     request.get(url, function (err, response, body){
       if (err) return (err);
       expect(response.statusCode).to.equal(200);
