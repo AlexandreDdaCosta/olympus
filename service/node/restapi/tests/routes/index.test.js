46ae7d66c2ac83ab317f15c96a168258ff813560
Alex da Costa <alex_investor@yahoo.com>
2022-04-25 00:03:31 -0400

    Debugged http/https tests for restapi root path

--- service/node/restapi/tests/routes/index.test.js
+++ service/node/restapi/tests/routes/index.test.js
@@ -1,18 +1,16 @@
-const fs = require('fs'); 
-const https = require('https'); 
 const request = require('request');
 
-//
+const url = 'http://zeus:8889/';
+const message = 'Olympus back-end API listening for requests via express/node.js.'
+
+/*
 Test direct backend connection, bypassing nginx
 Command line: curl localhost:8889
-//
-
-const url = 'http://zeus:8889/api/';
-const message = 'Olympus back-end API listening for requests via express/node.js.'
+*/
 
 describe('Connection to HTTP index page of API, node port', () => {
   test('Should return HTTP code 200 with JSON reply including a "message" key.', () => {
-    request.get(url, function (err, response, body){
+    request.get(url, function (err, response){
       if (err) return (err);
       expect(response.statusCode).toBe(200);
       jsonobject = JSON.parse(response.body);
@@ -22,26 +20,30 @@ describe('Connection to HTTP index page of API, node port', () => {
 });
 
 /*
-var options = { 
+Test nginx proxy connection
+Command line: curl https://zeus:4443 ????? ALEX
+curl -k https://zeus:4443 -v -key /etc/ssl/localcerts/client-key.pem -cacert /etc/ssl/certs/ca-crt-supervisor.pem.pem -cert /etc/ssl/localcerts/client-crt.pem
+*/
+
+const fs = require('fs'); 
+const https = require('https'); 
+const options = { 
     hostname: 'zeus', 
     port: 4443, 
-    path: '/api', 
-    method: 'get',
+    path: '/', 
+    method: 'GET',
     key: fs.readFileSync('/etc/ssl/localcerts/client-key.pem'), 
     cert: fs.readFileSync('/etc/ssl/localcerts/client-crt.pem'), 
     ca: fs.readFileSync('/etc/ssl/certs/ca-crt-supervisor.pem.pem')
 }; 
-describe('Connection to HTTPS index page of API', function () {
-  it('Should return HTTP code 200 with JSON reply including a "message" key.', function (done) {
-    var request = https.request(options, function(response) { 
-      expect(response.statusCode).to.equal(200);
-      response.on('data', function(data) { 
-        jsonobject = JSON.parse(data);
-        expect(jsonobject['message']).to.equal(message);
-      }); 
-      done();
+describe('Connection to HTTPS index page of API', () => {
+  test('Should return HTTP code 200 with JSON reply including a "message" key.', () => {
+    const req = https.request(options, function (err, resp) { 
+      if (err) return (err);
+      expect(response.statusCode).toBe(200);
+      jsonobject = JSON.parse(resp.body);
+      expect(jsonobject['message']).toBe(message);
     });
-    request.end();
+    req.end();
   }); 
-}); 
-*/
+});
