cf3d8b2273d5f824dff8d3f0fa83553a7a328164
Alex da Costa <alex_investor@yahoo.com>
2022-04-28 23:19:25 -0400

    Removed extra arguments; basic _connect subroutine

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -8,16 +8,21 @@ import ast, pymongo
 
 from olympus import MONGO_URL
 
-def insert_object(database,collection,datasource_name,key_name,key,issue_epoch_date,object):
+def insert_object(database,collection,object):
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     coll = db[collection]
     #recid = coll.insert_one(object)
     return True
 
-def remove_object(database,collection,datasource_name,key_name,query):
+def remove_object(database,collection,query):
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     coll = db[collection]
     recid = coll.delete_one(query)
     return True
+
+def _connect(database, collection):
+    client = pymongo.MongoClient(MONGO_URL)
+    db = client[database]
+    return db[collection]
