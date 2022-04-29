63aaaddd763b91c081aff82eb5a93358fe494f03
Alex da Costa <alex_investor@yahoo.com>
2022-04-28 20:03:41 -0400

    Extra debugging printing

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -7,6 +7,12 @@ Tools for handling MongoDB operations
 def insert_object(database,collection,datasource_name,key_name,key,issue_epoch_date):
     f = open("/tmp/narfie", "a")
     f.write('TEST1')
+    f.write(database)
+    f.write(collection)
+    f.write(datasource_name)
+    f.write(key_name)
+    f.write(key)
+    f.write(issue_epoch_date)
     f.close()
     # object: [{ "DataSource": {{ datasource_name }}, "KeyName": {{ datasource['KeyName'] }}, "Key": {{ datasource['Key'] }}, "IssueEpochDate": {{ datasource['IssueEpochDate'] }} }]
     return True
