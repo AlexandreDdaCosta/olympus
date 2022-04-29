2ecafae2dda367e9603d18c582254b3f0d10f5e3
Alex da Costa <alex_investor@yahoo.com>
2022-04-28 22:50:13 -0400

    Updated print statements

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -9,24 +9,22 @@ import ast, pymongo
 from olympus import MONGO_URL
 
 def insert_object(database,collection,datasource_name,key_name,key,issue_epoch_date,object):
-    f = open("/tmp/narfie", "a")
-    f.write('TEST1\n')
-    f.write(object+'\n')
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     coll = db[collection]
     record = ast.literal_eval(object)
-    f.write(str(record)+'\n')
-    f.write(str(type(record))+'\n')
     #recid = coll.insert_one(record)
-    f.write(str(recid)+'\n')
-    f.close()
     return True
 
 def remove_object(database,collection,datasource_name,key_name,query):
+    f = open("/tmp/narfie", "a")
+    f.write('TEST1\n')
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     coll = db[collection]
     record = ast.literal_eval(query)
+    f.write(str(query)+'\n')
+    f.write(str(type(query))+'\n')
     recid = coll.delete_one(record)
+    f.close()
     return True
