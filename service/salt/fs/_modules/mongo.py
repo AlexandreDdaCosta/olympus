6d6db40ea3ab4d8caf7e630012aeda2dab09c0a9
Alex da Costa <alex_investor@yahoo.com>
2022-04-28 22:36:11 -0400

    Attempt to find source of print failure

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -10,33 +10,20 @@ from olympus import MONGO_URL
 
 def insert_object(database,collection,datasource_name,key_name,key,issue_epoch_date,object):
     f = open("/tmp/narfie", "a")
-    f.write('TEST1')
-    f.write(database)
-    f.write(collection)
-    f.write(datasource_name)
-    f.write(key_name)
-    f.write(key)
-    f.write(str(issue_epoch_date))
-    f.write(object)
+    f.write('TEST1\n')
+    f.write(object+'\n')
     # object: [{ "DataSource": {{ datasource_name }}, "KeyName": {{ datasource['KeyName'] }}, "Key": {{ datasource['Key'] }}, "IssueEpochDate": {{ datasource['IssueEpochDate'] }} }]
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     coll = db[collection]
     record = ast.literal_eval(object)
-    f.write(str(record))
-    f.write(str(type(record)))
+    f.write(str(record)+'\n')
+    f.write(str(type(record))+'\n')
     recid = coll.insert_one(record)
-    f.write(str(recid))
+    f.write(str(recid)+'\n')
     f.close()
     return True
 
 def remove_object(database,collection,datasource_name,query):
-    f = open("/tmp/narfie", "a")
-    f.write('TEST2')
-    f.write(database)
-    f.write(collection)
-    f.write(datasource_name)
-    f.write(query)
-    f.close()
     # query: [{ "DataSource": {{ datasource_name }} }]
     return True
