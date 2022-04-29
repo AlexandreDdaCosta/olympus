cf667a44590b35d97b7b5a76059852fb8ed71acc
Alex da Costa <alex_investor@yahoo.com>
2022-04-28 23:25:56 -0400

    Debug complete, final code installation

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -9,22 +9,16 @@ import ast, pymongo
 from olympus import MONGO_URL
 
 def insert_object(database,collection,object):
-    #client = pymongo.MongoClient(MONGO_URL)
-    #db = client[database]
-    #coll = db[collection]
     coll = _connect(database, collection)
     recid = coll.insert_one(object)
     return True
 
 def remove_object(database,collection,query):
-    #client = pymongo.MongoClient(MONGO_URL)
-    #db = client[database]
-    #coll = db[collection]
     coll = _connect(database, collection)
     recid = coll.delete_one(query)
     return True
 
-def _connect(database, collection):
+def _connect(database,collection):
     client = pymongo.MongoClient(MONGO_URL)
     db = client[database]
     return db[collection]
