5b28f0fbfd561977710cba7d80c8d5139eabe591
Alex da Costa <alex_investor@yahoo.com>
2022-05-01 00:36:55 -0400

    After much trial and error, got a connect test working.

--- service/node/restapi/tests/models/connect.test.js
+++ service/node/restapi/tests/models/connect.test.js
@@ -1,171 +1,31 @@
 // sudo su -s /bin/bash -c 'export NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript; cd /srv/www/node/restapi; npm test ./tests/models/connect.test.js' node
 
-const async = require('async');
-const fs = require('fs'); 
-const MongoClient = require('mongodb').MongoClient;
-const test_collection = 'test'
-const url = 'mongodb://127.0.0.1:27017/' + test_collection;
+const {MongoClient} = require('mongodb');
 
-describe('MongoDB connection', () => {
-  test('Connect to test database.', () => {
-    MongoClient.connect(url, (error, database) => {
-      if (error) throw new Error(error);
-      async.waterfall([
+let test_collection = 'connect.test'
+let test_database = 'test_node'
+let url = 'mongodb://127.0.0.1:27017/';
 
+describe('insert', () => {
+  let connection;
+  let db;
 
+  beforeAll(async () => {
+    connection = await MongoClient.connect(url, {});
+    db = await connection.db(test_database);
+  });
 
-      ]);
-      database.close();
-    });
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('Dopey test', async () => {
+    expect('1').toEqual('1');
   });
 });
-
 
 /*
 
-function removeCollection(db,collectionName,callback) {
-
-  async.waterfall([
-
-    // Get collection
-    function(cb) {
-      db.collection(collectionName,function(err,collection) {
-        if (err) throw err;
-        cb(null,collection);
-      });
-    },
-
-    // Count collection current
-    function(collection,cb) {
-      collection.count(function(err,count) {
-        console.log(
-          "%s collection has %s documents",
-          collectionName,
-          count
-        );
-        cb(null,collection);
-      });
-    },
-
-    // Remove collection documents
-    function(collection,cb) {
-      collection.remove({},function(err,removed) {
-        console.log(
-          "%s collection removed %s documents",
-          collectionName,
-          removed
-        );
-        cb(null,collection);
-      });
-    },
-
-  ],function(err,collection) {
-
-    collection.count(function(err,count) {
-      console.log(
-        "%s collection now has %s documents",
-        collectionName,
-        count
-      );
-      callback(err);
-    });
-
-  });
-}
-
-// Main flow after connection
-MongoClient.connect('mongodb://localhost/test',function(err,db) {
-
-  if (err) throw err;
-
-  async.waterfall([
-
-    // Set up a collection
-    function(cb) {
-      var collectionName = "sample";
-
-      db.collection(collectionName,function(err,collection) {
-        cb(null,collection,collectionName);
-      });
-    },
-
-    // Insert some things
-    function(collection,collectionName,cb) {
-      async.eachSeries([1,2,3,4,5,6,7,8],function(item,complete) {
-        collection.insert({ "item": item },function(err) {
-          if (err) throw err;
-          complete();
-        });
-      },function(err) {
-        // When all are complete
-        cb(null,collectionName);
-      });
-    },
-
-    // Call your remove method
-    function(collectionName,cb) {
-
-      removeCollection(db,collectionName,function(err) {
-        if (err) throw err;
-
-        cb();
-      });
-
-    }
-
-  ]);
-
-});
-
-
-
-
-
-Initialize test commands
-
-const insertDocuments = (db, callback) => {
-  var collection = db.collection(test_collection);
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    expect(err).toBe(null);
-    expect(result.result.ok).toBe(1);
-    expect(result.result.n).toBe(3);
-    expect(result.insertedCount).toBe(3);
-    expect(result.ops.length).toBe(3);
-    expect(result.ops[0]['a']).toBe(1);
-    expect(result.ops[1]['a']).toBe(2);
-    expect(result.ops[2]['a']).toBe(3);
-    console.log("Inserted three documents into test collection.");
-  });
-}
-
-const removeCollection = (client, callback) => {
-  var collection = client.collection(test_collection);
-  collection.remove( function(err, result) {
-    expect(err).toBe(null);
-    expect(result.result.ok).toBe(1);
-    console.log("Dropped the test collection.");
-    callback();
-  });    
-}
-
-Run test commands
-
-describe('MongoDB basic connection and test database and collection operations', () => {
-  test('Connect to test database.', () => {
-    MongoClient.connect(url, (err, client) => {
-      if (err) throw new Error(err);
-//      removeCollection(client, function() {});
-//        insertDocuments(client, function() {
-      client.close();
-    });
-  });
-});
-*/
-
-/*
-ALEX
 
 var findDocuments = function(db, callback) {
   var collection = db.collection(test_collection);
