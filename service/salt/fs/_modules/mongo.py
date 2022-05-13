2798753dac0072af965c3762ebb7aa7d4bbd3fec
Alex da Costa <alex_investor@yahoo.com>
2022-05-13 00:31:18 -0400

    Extra array brackets around dict

--- service/salt/fs/_modules/mongo.py
+++ service/salt/fs/_modules/mongo.py
@@ -30,7 +30,7 @@ def user(username,password,admin=False,roles=None):
         new_roles = []
         for role_ordered_dict in roles:
             for database, role in role_ordered_dict.items():
-                role_dict = [{'role': role, 'db': database}]
+                role_dict = {'role': role, 'db': database}
                 new_roles.append(role_dict)
                 break
         roles = new_roles
