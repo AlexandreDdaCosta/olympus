cbad80b8c7e93001ff3b28b5c3182c353279ea0d
Alex da Costa <alex_investor@yahoo.com>
2022-05-15 03:22:15 -0400

    Updated NODE_PATH as in start-up to include local library

--- service/node/restapi/tests/test_source.sh
+++ service/node/restapi/tests/test_source.sh
@@ -1,2 +1,2 @@
 export NODE_CONFIG_DIR=/srv/www/node/restapi/config
-export NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript
+export NODE_PATH=/srv/www/node/restapi/lib:/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript
