0964af9df416a438e82a790da7ad34ead4166cfb
Alex da Costa <alex_investor@yahoo.com>
2022-06-02 20:16:39 -0400

    Reversed files inadvertently

--- service/node/restapi/tests/routes/auth.test.js
+++ service/node/restapi/tests/routes/auth.test.js
@@ -1,19 +1,21 @@
 // sudo su -s /bin/bash -c 'source /srv/www/node/restapi/tests/test_source.sh; cd /srv/www/node/restapi; npm test ./tests/models/auth.test.js' node
 
-const {MongoClient} = require('mongodb');
-const argon2 = require('argon2');
-const config = require('config');
 const fs = require('fs');
 const os = require('os');
- 
-const hashing_config = { parallelism: config.get('argon2.parallelism'), memoryCost: config.get('argon2.memory_cost'), timeCost: config.get('argon2.time_cost') }
-const test_collection = config.get('mongodb.collection_auth_users');
+const https = require('https'); 
 
-describe('check_password', () => {
-  let connection;
-  let db;
+describe('Login, refresh token, and logout from node restapi.', () => {
+  let message;
+  let options;
   let password;
-  let restapi_password;
+
+  options = { 
+    hostname: 'zeus', 
+    port: 4443, 
+    key: fs.readFileSync('/etc/ssl/localcerts/client-key.pem'), 
+    cert: fs.readFileSync('/etc/ssl/localcerts/client-crt.pem'), 
+    ca: fs.readFileSync('/etc/ssl/certs/ca-crt-supervisor.pem.pem')
+  }; 
 
   if (os.userInfo().username != config.get('mongodb.user')) {
     throw new Error('Test must be run under run user '+config.get('mongodb.user'));
@@ -25,29 +27,20 @@ describe('check_password', () => {
     throw new Error(err);
     process.exit(1);
   }
-  try {
-    restapi_password = fs.readFileSync(config.get('restapi.password_file'), 'utf8');
-  } catch (err) {
-    throw new Error(err);
-    process.exit(1);
-  }
 
-  beforeAll(async () => {
-    const uri = 'mongodb://'+config.get('mongodb.user')+':'+password+'@'+config.get('mongodb.host')+':'+config.get('mongodb.port')+'/'+config.get('mongodb.database_auth');
-    connection = await MongoClient.connect(uri, {});
-    db = await connection.db(config.get('mongodb.database_default'));
+  it('User authentication, get access and refresh tokens.', async () => {
+    message = 'TEST MESSAGE'
+    options['method'] = 'POST';
+    options['path'] = '/auth';
+    const https_request = https.request(options, (response) => { 
+      expect(response.statusCode).toBe(200);
+      response.on('data', (d) => {
+        jsonobject = JSON.parse(d);
+        expect(jsonobject['message']).toBe(message);
+      });
+    });
+    https_request.end();
   });
 
-  afterAll(async () => {
-    var collection = db.collection(test_collection);
-    await connection.close();
-  });
-
-  it('Verify hashed node password in mongodb versus clear text on disk', async () => {
-    var collection = db.collection(test_collection);
-    var searchedItem = await collection.findOne({Username : config.get('mongodb.user')});
-    var hashed_password = searchedItem['Password'];
-    var result = await argon2.verify(hashed_password, restapi_password, hashing_config);
-    expect(result).toEqual(true);
-  });
 });
+
