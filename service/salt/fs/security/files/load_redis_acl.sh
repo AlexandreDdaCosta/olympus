7ee0d4ac0901cd2afc8a0d2158bc043add5bb9c7
Alex da Costa <alex_investor@yahoo.com>
2022-06-15 14:45:52 -0400

    Missing quotes to identify empty variable

--- service/salt/fs/security/files/load_redis_acl.sh
+++ service/salt/fs/security/files/load_redis_acl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $REDIS_DEFAULT_PASSWORD != '' ]
+if [ "$REDIS_DEFAULT_PASSWORD" != '' ]
 then
     echo -e "auth default $REDIS_DEFAULT_PASSWORD\nacl load" | /usr/bin/redis-cli
 else
