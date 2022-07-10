17f5d345fa7d9c1fb79c4dd0da376cbd8bcc3d5d
Alex da Costa <alex_investor@yahoo.com>
2022-07-10 01:43:25 -0400

    Completed daily price quote with database writes and update of stale date Expanding test process to check for properly generated data when stale

--- core/lib/securities/equities/data/price.py
+++ core/lib/securities/equities/data/price.py
@@ -1,9 +1,8 @@
-import collections, datetime, json, re, urllib.request, wget
+import collections, datetime, json, os, re, urllib.request, wget
 
 from datetime import datetime as dt
 from datetime import timedelta
 from dateutil.parser import parse
-from pytz import timezone
 
 from olympus import USER, User
 
@@ -24,17 +23,17 @@ collection are arranged as follows:
     "End Time": "<Time stamp for last interval stored. One minute resolution.>"
     "Quotes" : {
         "<Time stamp, one minute resolution" : {
-            "open" : <quote>,
-            "high" : <quote>,
-            "low" : <quote>,
-            "close" : <quote>,
+            "Open" : <quote>,
+            "High" : <quote>,
+            "Low" : <quote>,
+            "Close" : <quote>,
+            "Adjusted Close" : <quote>,
             [
-            "adjusted open" : <quote>,
-            "adjusted high" : <quote>,
-            "adjusted low" : <quote>,
-            "adjusted close" : <quote>,
+            "Adjusted Open" : <quote>,
+            "Adjusted High" : <quote>,
+            "Adjusted Low" : <quote>,
             ]
-            "volume" : "6629600"
+            "Volume" : "6629600"
 },
 <more intervals>
 ...,
@@ -43,7 +42,7 @@ collection are arranged as follows:
 
 Notes:
 
-1. Adjusted prices are included only when they differ from as-traded prices.
+1. Adjusted prices within brackets are included only when they differ from as-traded prices.
 
 '''
 
@@ -54,13 +53,12 @@ class Daily(data.Connection):
         self.symbol_reader = symbols.Read(username,**kwargs)
 
     def quote(self,symbol,**kwargs):
-        symbol = symbol.upper()
+        symbol = str(symbol).upper()
         symbol_data = self.symbol_reader.get_symbol(symbol)
         regen = kwargs.get('regen',False)
         start_date = kwargs.get('start_date',None)
         end_date = kwargs.get('end_date',None)
-        tz = timezone('EST')
-        now = dt.now(tz)
+        now = dt.now().astimezone()
         price_collection = 'price.' + symbol
         period1 = '0'
         period2 = '9999999999' # To the present day, and beyooond
@@ -85,9 +83,9 @@ class Daily(data.Connection):
             weekday_no = now.weekday()
             if weekday_no < 5:
                 # weekday
-                midnight = "%d-%02d-%02d 00:00:00.000000-05:00" % (now.year,now.month,now.day)
+                midnight = "%d-%02d-%02d 00:00:00.000000-04:00" % (now.year,now.month,now.day)
                 midnight_object = parse(midnight)
-                four_pm = "%d-%02d-%02d 16:00:00.000000-05:00" % (now.year,now.month,now.day)
+                four_pm = "%d-%02d-%02d 16:00:00.000000-04:00" % (now.year,now.month,now.day)
                 four_pm_object = parse(four_pm)
                 if now <= four_pm_object and refresh_date_object < midnight_object:
                     stale = True
@@ -96,43 +94,39 @@ class Daily(data.Connection):
             else:
                 # weekend
                 last_friday = now - timedelta(days=(weekday_no-4))
-                last_friday_string = "%d-%02d-%02d 16:00:00.000000-05:00" % (last_friday.year,last_friday.month,last_friday.day)
+                last_friday_string = "%d-%02d-%02d 16:00:00.000000-04:00" % (last_friday.year,last_friday.month,last_friday.day)
                 last_friday_object = parse(last_friday_string)
                 if refresh_date_object < last_friday_object:
                     stale = True
         elif interval_data is None:
-            stale = True
-        stale = True # ALEX
+            regen = True
         if regen is False and stale is True:
             if self.end_date_stored is not None:
-                date_pieces = self.end_date_stored.rstrip().split('-')
                 year,month,day = map(int,self.end_date_stored.rstrip().split('-'))
                 period1 = str(int(dt(year, month, day, 0, 0, 0).timestamp()))
-            url = 'https://query1.finance.yahoo.com/v7/finance/download/' + str(symbol) + '?period1=' + period1 + '&period2=' + period2 + '&interval=1d&events=history&includeAdjustedClose=true'
-            target_file = self.download_directory()+str(symbol)+'-daily.csv'
+            url = 'https://query1.finance.yahoo.com/v7/finance/download/' + symbol + '?period1=' + period1 + '&period2=' + period2 + '&interval=1d&events=history&includeAdjustedClose=true'
+            target_file = self.download_directory()+symbol+'-daily.csv'
             response = urllib.request.urlretrieve(url,target_file)
             with open(target_file,'r') as f:
                 first_line = f.readline()
                 if not re.match(r'^Date,Open,High,Low,Close,Adj Close,Volume',first_line):
                     raise Exception('First line of symbol daily data .csv file does not match expected format.')
-                compare_line = f.readline().rstrip()
-                pieces = compare_line.rstrip().split(',')
-                adjustment_multiplier = None
-                if float(pieces[4]) != float(pieces[5]):
-                    adjustment_multiplier = str("%.4f" % (float(pieces[5]) / float(pieces[4])))
-                if adjustment_multiplier is not None:
-                    if adjustment_mutiplier not in interval_data['Quotes'][self.end_date_stored] or adjustment_mutiplier != interval_data['Quotes'][self.end_date_stored]['adjustment_multiplier']:
+                if self.end_date_stored is not None:
+                    compare_line = f.readline().rstrip()
+                    pieces = compare_line.rstrip().split(',')
+                    if interval_data['Quotes'][self.end_date_stored]['Adjusted Close'] != str("%.2f" % float(pieces[5])):
                         # There's been a price adjustment, regenerate everything
                         regen = True
-                if not regen:
+                if regen is False:
                     for line in f:
-                        json_quote, interval_date = self._parse(line)
-                        collection.update_one({'Interval': '1d'},{'Quotes.'+interval_date: json_quote}, upsert=True)
+                        (json_quote,interval_date) = self._parse(line)
+                        collection.update_one({'Interval': '1d'},{ "$set": {'Quotes.'+interval_date: json_quote}}, upsert=True)
                         interval_data['Quotes'][interval_date] = json_quote
                     collection.update_one({'Interval': '1d'},{ "$set":  {'End Date': self.end_date_stored, 'Start Date': self.start_date_stored, 'Time': str(now)}})
+            os.remove(target_file)
         if regen is True:
-            url = 'https://query1.finance.yahoo.com/v7/finance/download/' + str(symbol) + '?period1=0&period2=' + period2 + '&interval=1d&events=history&includeAdjustedClose=true'
-            target_file = self.download_directory()+str(symbol)+'-daily.csv'
+            url = 'https://query1.finance.yahoo.com/v7/finance/download/' + symbol + '?period1=0&period2=' + period2 + '&interval=1d&events=history&includeAdjustedClose=true'
+            target_file = self.download_directory()+symbol+'-daily.csv'
             response = urllib.request.urlretrieve(url,target_file)
             with open(target_file,'r') as f:
                 first_line = f.readline()
@@ -141,7 +135,7 @@ class Daily(data.Connection):
                     raise Exception('First line of symbol daily data .csv file does not match expected format.')
                 json_quotes = {}
                 for line in f:
-                    json_quote, interval_date = self._parse(line)
+                    (json_quote,interval_date) = self._parse(line)
                     json_quotes[interval_date] = json_quote
                 write_dict = {}
                 write_dict['Time'] = str(now)
@@ -153,6 +147,7 @@ class Daily(data.Connection):
                 collection.insert_one(write_dict)
                 interval_data = collection.find_one({ 'Interval': '1d'})
                 returndata = write_dict['Quotes']
+            os.remove(target_file)
 
         if returndata is None:
             returndata = interval_data['Quotes']
@@ -171,35 +166,45 @@ class Daily(data.Connection):
         interval_date = str(pieces[0])
         if (self.start_date_stored is None or self.start_date_stored >= interval_date):
             self.start_date_stored = interval_date
-            if (self.end_date_stored is None or self.end_date_stored <= interval_date):
-                self.end_date_stored = interval_date
-            json_quote['open'] = str("%.2f" % float(pieces[1]))
-            json_quote['high'] = str("%.2f" % float(pieces[2]))
-            json_quote['low'] = str("%.2f" % float(pieces[3]))
-            json_quote['close'] = str("%.2f" % float(pieces[4]))
-            json_quote['volume'] = pieces[6]
-            if (float(pieces[4]) != float(pieces[5])):
-                json_quote['adjustment_multiplier'] = str("%.4f" % (float(pieces[5]) / float(pieces[4])))
-            return json_quote, interval_date
+        if (self.end_date_stored is None or self.end_date_stored <= interval_date):
+            self.end_date_stored = interval_date
+        json_quote['Open'] = str("%.2f" % float(pieces[1]))
+        json_quote['High'] = str("%.2f" % float(pieces[2]))
+        json_quote['Low'] = str("%.2f" % float(pieces[3]))
+        json_quote['Close'] = str("%.2f" % float(pieces[4]))
+        json_quote['Volume'] = pieces[6]
+        json_quote['Adjusted Close'] = str("%.2f" % float(pieces[5]))
+        if (float(pieces[4]) != float(pieces[5])):
+            adjustment = float(pieces[5]) / float(pieces[4])
+            json_quote['Adjusted Open'] = str("%.2f" % (float(pieces[1]) * adjustment))
+            json_quote['Adjusted High'] = str("%.2f" % (float(pieces[2]) * adjustment))
+            json_quote['Adjusted Low'] = str("%.2f" % (float(pieces[3]) * adjustment))
+        return json_quote, interval_date
 
-    def latest(self,symbol,**kwargs):
+class Latest(data.Connection):
+
+    def __init__(self,username=USER,**kwargs):
+        super(Daily,self).__init__(username,**kwargs)
+        self.symbol_reader = symbols.Read(username,**kwargs)
+
+    def quote(self,symbol,**kwargs):
         # Complete price quote for latest trading day
         url = URLS['AlphaVantage'] + 'CHLVRDAEA445JOCB' + '&function=GLOBAL_QUOTE&symbol=' + symbol
         request = urllib.request.urlopen(url)
         quote = json.loads(re.sub(r'^\s*?\/\/\s*',r'',request.read().decode("utf-8")))
         quote = quote['Global Quote']
         latest = {}
-        latest['change'] = quote['09. change']
-        latest['change percent'] = quote['10. change percent']
-        latest['close'] = quote['05. price']
-        latest['date'] = quote['07. latest trading day']
-        latest['last'] = quote['05. price']
-        latest['low'] = quote['04. low']
-        latest['high'] = quote['03. high']
-        latest['open'] = quote['02. open']
-        latest['previous'] = quote['08. previous close']
-        latest['symbol'] = symbol
-        latest['volume'] = quote['06. volume']
+        latest['Change'] = quote['09. change']
+        latest['Change Percent'] = quote['10. change percent']
+        latest['Close'] = quote['05. price']
+        latest['Date'] = quote['07. latest trading day']
+        latest['Last'] = quote['05. price']
+        latest['Low'] = quote['04. low']
+        latest['High'] = quote['03. high']
+        latest['Open'] = quote['02. open']
+        latest['Previous'] = quote['08. previous close']
+        latest['Symbol'] = symbol
+        latest['Volume'] = quote['06. volume']
         return latest
 
 class Intraday(alphavantage.Connection):
--- core/lib/securities/equities/data/test/price.py
+++ core/lib/securities/equities/data/test/price.py
@@ -5,6 +5,7 @@ import json, sys, unittest
 from olympus import USER
 from olympus.securities.equities.data.symbols import SymbolNotFoundError
 
+import olympus.securities.equities.data as data
 import olympus.securities.equities.data.price as price
 import olympus.testing as testing
 
@@ -24,13 +25,29 @@ class TestPrice(testing.Test):
             username = self.validRunUser(USER)
         self.daily = price.Daily(username)
         self.intraday = price.Intraday(username)
+        self.mongo_data = data.Connection(username)
 
     def test_daily(self):
-    #    with self.assertRaises(SymbolNotFoundError):
-    #        quotes = self.quote.daily(TEST_SYMBOL_FAKE)
-    #    quotes = self.quote.daily(TEST_SYMBOL_ONE,regen=True)
-    #    quotes_noregen = self.quote.daily(TEST_SYMBOL_ONE)
-        quotes_noregen = self.daily.quote('ZIM')
+        TEST_SYMBOL_ONE = 'ZIM'
+        with self.assertRaises(SymbolNotFoundError):
+            quotes = self.daily.quote(TEST_SYMBOL_FAKE)
+        quotes = self.daily.quote(TEST_SYMBOL_ONE,regen=True)
+        # Remove the last price record by date, then get the quote again. Check that the record was restored.
+        price_collection = 'price.' + TEST_SYMBOL_ONE
+        collection = self.mongo_data.db[price_collection]
+        interval_data = collection.find_one({ 'Interval': '1d' },{ '_id': 0, 'Interval': 0 })
+        last_date = None
+        previous_date = None
+        for quote_date in interval_data['Quotes']:
+            previous_date = last_date
+            if last_date is None or last_date < quote_date:
+                last_date = quote_date
+        if previous_date is not None:
+            year,month,day = map(int,previous_date.split('-'))
+            time_string = "%d-%02d-%02d 00:00:00.000000-04:00" % (year,month,day)
+            collection.update_one({ 'Interval': '1d' },{ "$unset": { 'Quotes.'+last_date: 1 }})
+            collection.update_one({ 'Interval': '1d' },{ "$set":  { 'End Date': previous_date, 'Time': time_string }})
+        #quotes_noregen = self.daily.quote(TEST_SYMBOL_ONE)
     
     #def test_intra_day(self):
     #    quotes = self.intraday.quote(TEST_SYMBOL_ONE)
