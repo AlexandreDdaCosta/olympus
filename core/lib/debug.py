42faf917f11adb38add07ccaaf687b95cccf0ab4
Alex da Costa <alex_investor@yahoo.com>
2022-11-13 00:48:43 -0500

    Matched notation for all market key rules Added ability to send all debug output to file. Added ability to disable program pause for a debug statement. More debug statements added to market key main process.

--- core/lib/debug.py
+++ core/lib/debug.py
@@ -1,4 +1,4 @@
-import inspect, json, os, sys
+import inspect, json, os, subprocess, sys, time
 
 from argparse import ArgumentParser
 
@@ -11,6 +11,7 @@ class DebuggerArgs():
     def add_arguments(self):
         parser = ArgumentParser(sys.argv)
         parser.add_argument("-D","--debug",action='store_true',help="Executes built-in pauses with data dumps suitable for deep debugging")
+        parser.add_argument("-F","--to_file",action='store_true',help="Sends debugging output to a prenamed output file; disables all processing pauses")
         parser.add_argument("-L","--debug_level",choices=DEBUG_LEVELS,default=DEFAULT_DEBUG_LEVEL,help="Get more debugging output by increasing this setting; levels " + str(DEBUG_LEVELS) + " available; default is '" + str(DEFAULT_DEBUG_LEVEL)  + "'",type=int)
         return parser
 
@@ -23,6 +24,13 @@ class Debugger():
             debugger_arguments = DebuggerArgs()
             parser = debugger_arguments.add_arguments()
             self.args, unknown = parser.parse_known_args()
+        if self.args.to_file:
+            if not hasattr(self.args, 'filehandle'):
+                file_name = '/tmp/debug.' + os.path.basename(sys.argv[0]) + '.' + str(time.time())
+                self.args.filehandle = open(file_name,"a")
+                os.chmod(file_name, 0o0440)
+                subprocess.run(["/bin/bash -c 'read -n 1 -s -p \"Output to file " + file_name + "; press any key to continue ...\"'"], stdout=subprocess.PIPE, shell=True)
+                print()
 
     def debug_args(self):
         return self.args
@@ -30,25 +38,32 @@ class Debugger():
     def debug(self,debug_level=DEFAULT_DEBUG_LEVEL,message='Debug message left blank.',data=None,**kwargs):
         if not self.args.debug:
             return
-        caller_frame = sys._getframe(1)
-        caller_frameinfo = inspect.getframeinfo(caller_frame)
-        sourcefile = inspect.getsourcefile(caller_frame)
         if debug_level <= self.args.debug_level:
             if self.args.debug_level == 1:
-                print("{message}".format(message=message))
+                self._print_debug_output("{message}".format(message=message))
             else:
-                print("{message} [{sourcefile}, {lineno}]".format(message=message, sourcefile=sourcefile, lineno=caller_frameinfo.lineno))
+                caller_frame = sys._getframe(1)
+                caller_frameinfo = inspect.getframeinfo(caller_frame)
+                sourcefile = inspect.getsourcefile(caller_frame)
+                self._print_debug_output("{message} [{sourcefile}, {lineno}]".format(message=message, sourcefile=sourcefile, lineno=caller_frameinfo.lineno))
             if data is not None and self.args.debug_level >= 2:
                 if isinstance(data, object) and hasattr(data, '__dict__'):
-                    print(json.dumps(vars(data), default=str, indent=4, sort_keys=True))
+                    self._print_debug_output(json.dumps(vars(data), default=str, indent=4, sort_keys=True))
                 else:
-                    print(json.dumps(data, default=str, indent=4, sort_keys=True))
-                if debug_level > 1 and debug_level == self.args.debug_level:
-                    import subprocess
-                    result = subprocess.run(["/bin/bash -c 'read -n 1 -s -p \"<<<<\" input; echo $input'"], stdout=subprocess.PIPE, shell=True)
-                    if b'q' in result.stdout:
-                        print('\nDebugger halting due to user input; exiting ...')
-                        quit()
-                    print()
-                else:
-                    print('.')
+                    self._print_debug_output(json.dumps(data, default=str, indent=4, sort_keys=True))
+                if not self.args.to_file:
+                    no_pause = kwargs.get('no_pause',False)
+                    if debug_level > 1 and debug_level == self.args.debug_level and no_pause is False:
+                        result = subprocess.run(["/bin/bash -c 'read -n 1 -s -p \"<<<< (q=quit) \" input; echo $input'"], stdout=subprocess.PIPE, shell=True)
+                        if b'q' in result.stdout:
+                            print('\nDebugger halting due to user input; exiting ...')
+                            quit()
+                        print()
+                    else:
+                        print('.')
+
+    def _print_debug_output(self,message):
+        if self.args.to_file:
+            self.args.filehandle.write(message + '\n')
+        else:
+            print(message)
