2ba54b89e9fd81c4d5e88cf42781b41cf879b913
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-05-03 01:42:20 -0400

    Added comments to both redis scripts used by SaltStack. Modified delete redis keys script to target only keys stored for REST api authentication.

--- service/salt/fs/security/files/delete_redis_restapi_auth.sh
+++ service/salt/fs/security/files/delete_redis_restapi_auth.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Clear out REST api tokens from redis
+# Necessary when REST api passwords get regenerated
+
+# Without this script, REST access will be denied until the old
+# tokens expire.
+
 if [ "$REDIS_DEFAULT_PASSWORD" != '' ]
 then
-    echo -e "auth default $REDIS_DEFAULT_PASSWORD\nflushall" | /usr/bin/redis-cli
-    echo -e "auth default $REDIS_DEFAULT_PASSWORD\nflushall" | /usr/bin/redis-cli
+    declare -a tokenarray
+    readarray -t tokenarray < <(echo -e "auth default $REDIS_DEFAULT_PASSWORD\nKEYS '*:restapi:token'" | /usr/bin/redis-cli)
+    for i in ${!tokenarray[@]}
+    do
+	if [[ ${tokenarray[i]}  =~ ^.*:restapi:token$ ]]
+	then
+            echo -e "auth default $REDIS_DEFAULT_PASSWORD\nDEL ${tokenarray[i]}" | /usr/bin/redis-cli
+        fi
+    done
+    declare -a autharray
+    readarray -t autharray < <(echo -e "auth default $REDIS_DEFAULT_PASSWORD\nKEYS 'restapi:auth:*'" | /usr/bin/redis-cli)
+    for i in ${!autharray[@]}
+    do
+	if [[ ${autharray[i]}  =~ ^restapi:auth:.*$ ]]
+	then
+            echo -e "auth default $REDIS_DEFAULT_PASSWORD\nDEL ${autharray[i]}" | /usr/bin/redis-cli
+        fi
+    done
 else
-    /usr/bin/redis-cli KEYS "restapi:auth*" | xargs /usr/bin/redis-cli DEL
-    /usr/bin/redis-cli KEYS "*restapi:token" | xargs /usr/bin/redis-cli DEL
+    /usr/bin/redis-cli KEYS "*:restapi:token" | xargs /usr/bin/redis-cli DEL
+    /usr/bin/redis-cli KEYS "restapi:auth:*" | xargs /usr/bin/redis-cli DEL
 fi
--- service/salt/fs/security/files/load_redis_acl.sh
+++ service/salt/fs/security/files/load_redis_acl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Load access control lists for redis
+
 if [ "$REDIS_DEFAULT_PASSWORD" != '' ]
 then
     echo -e "auth default $REDIS_DEFAULT_PASSWORD\nacl load" | /usr/bin/redis-cli
