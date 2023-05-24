2fe42d4a6eef4dedd64960474a7598f9168e2cf2
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-05-24 19:25:23 -0400

    Final ready-to-develop version of cookbook data model.

--- service/django/interface/apps/cookbook/models.py
+++ service/django/interface/apps/cookbook/models.py
@@ -2,41 +2,41 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-def only_filename(filename):
-    return filename
-
-
-class Graphic(models.Model):
-    url_path = models.URLField(max_length=255,
-                               null=False)
-    name = models.CharField(max_length=255,
-                            null=False)
-    caption = models.CharField(max_length=255)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['url_path', 'name'],
-                                    name='unique_graphic'),
-        ]
+class Media(models.Model):
+    description = models.CharField(max_length=255,
+                                   null=False)
+    url_path = models.URLField(max_length=200,
+                               null=False,
+                               unique=True)
 
     def __unicode__(self):
-        return '%s/%s' % (self.url_path, self.name)
+        return '%s' % (self.url_path)
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(null=False)
-    photo = models.ForeignKey(Graphic,
+    name = models.CharField(max_length=255,
+                            null=False,
+                            unique=True)
+    biography = models.TextField(null=False)
+    photo = models.ForeignKey(Media,
                               on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return '%s' % (self.name)
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(null=False)
+    name = models.CharField(max_length=255,
+                            null=False,
+                            unique=True)
+    book_order = models.IntegerField(MinValueValidator(1),
+                                     null=False,
+                                     unique=True)
 
 
 class Recipe(models.Model):
     course = models.ForeignKey(Course,
+                               null=False,
                                on_delete=models.CASCADE)
     course_order = models.IntegerField(MinValueValidator(1),
                                        null=False,
@@ -47,7 +47,29 @@ class Recipe(models.Model):
     postscript = models.TextField(null=True)
 
 
-class RecipeInstructions(models.Model):
+class RecipeAuthor(models.Model):
+    author = models.ForeignKey(Author,
+                               null=False,
+                               on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe,
+                               null=False,
+                               on_delete=models.CASCADE)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               null=False,
+                               on_delete=models.CASCADE)
+    ingredient_order = models.IntegerField(MinValueValidator(1),
+                                           null=False,
+                                           unique=True)
+    ingredient = models.CharField(db_index=True,
+                                  max_length=255,
+                                  null=False)
+    description = models.TextField(null=True)
+
+
+class RecipeInstruction(models.Model):
     recipe = models.ForeignKey(Recipe,
                                on_delete=models.CASCADE)
     instruction_order = models.IntegerField(MinValueValidator(1),
@@ -55,12 +77,22 @@ class RecipeInstructions(models.Model):
                                             unique=True)
     header = models.CharField(max_length=255,
                               null=True)
+    instruction = models.TextField(null=False)
 
 
-class RecipeGraphic(models.Model):
-    caption = models.CharField(max_length=255,
-                               null=True)
-    url = models.URLField(max_length=255)
+class RecipeMedia(models.Model):
+    recipe = models.ForeignKey(Recipe,
+                               null=False,
+                               on_delete=models.CASCADE)
+    media = models.ForeignKey(Media,
+                              null=False,
+                              on_delete=models.CASCADE)
+    bold_caption = models.CharField(max_length=255,
+                                    null=True)
+    caption = models.TextField(null=False)
 
-    def __unicode__(self):
-        return '%s' % (self.url)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['recipe', 'media'],
+                                    name='recipe_illustration'),
+        ]
