40de861ee44cb82c0bb0a6ee16b7ad95364de168
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-07-21 00:53:26 -0400

    Finished preparatory comment for pgadmin database maintenance.

--- service/salt/fs/_modules/pgadmin.py
+++ service/salt/fs/_modules/pgadmin.py
@@ -160,36 +160,33 @@ def pgadmin_db_user(username, email_address):
     """
     Populates and updates user configuration entries in pgadmin's
     SQLite database.
+
     Will need to touch these tables:
+
         a. user: Basic user information, including password
-           Table structure:
+           Table structure (required columns):
              id INTEGER NOT NULL
-               Auto added primary key
+               Auto generated primary key
              email VARCHAR(256)
              password VARCHAR
                Double-hash password using SECURITY_PASSWORD_SALT
              active BOOLEAN NOT NULL
-             confirmed_at DATETIME
              masterpass_check VARCHAR(256)
                106-character string (NEEDS RESEARCH)
              username VARCHAR(256) NOT NULL
              auth_source VARCHAR(256) DEFAULT 'internal' NOT NULL
              fs_uniquifier VARCHAR NOT NULL
                32-character random string
-             locked BOOLEAN DEFAULT 'false'
-             login_attempts INTEGER DEFAULT '0'
            Sample entry (one column per line):
              1,
              pgadmin@laikasden.com,
-             OMITTED
+             <OMITTED FOR LENGTH>
              1
-             None
-             OMITTED
+             <OMITTED FOR LENGTH>
              pgadmin@laikasden.com
              internal
              ca69ee8cfef348b6a3870dfe05d9bc89
-             0
-             0
+
         b. roles_users: pgadmin user should have admin role; user role
         for all others.
            Table structure:
@@ -203,20 +200,115 @@ def pgadmin_db_user(username, email_address):
            Sample entry (one column per line):
              1
              1
-        c. server: pgadmin user entries for local postgres server and
+           Notes:
+             - One role per user
+             - All users are of type "User" except for the admin user.
+
+        c. servergroup: Server UI group entries for all users.
+           Table structure:
+             id INTEGER NOT NULL
+               Auto generated primary key
+             user_id INTEGER NOT NULL
+               FOREIGN KEY(user_id) REFERENCES user (id)
+             name VARCHAR(128) NOT NULL
+           Sample entry (one column per line):
+             1
+             1
+             Interface
+           Notes:
+             - Create "interface" group for front-end databases
+
+        d. server: Admin user entries for local postgres server and
         databases.
-        d. sharedserver: Entries for all non-pgadmin users (pointing
-        back to "server")
-        e. servergroup: Server UI group entries for all users.
+           Table structure (required columns):
+             id INTEGER NOT NULL
+               Auto generated primary key
+             user_id INTEGER NOT NULL
+             servergroup_id INTEGER NOT NULL
+             name VARCHAR(128) NOT NULL
+             host VARCHAR(128)
+             port INTEGER
+             maintenance_db VARCHAR(64)
+             username VARCHAR(64)
+             comment VARCHAR(1024)
+             db_res VARCHAR
+             shared BOOLEAN,
+             connection_params JSON
+             FOREIGN KEY(user_id) REFERENCES user (id)
+             FOREIGN KEY(servergroup_id) REFERENCES servergroup (id)
+           Sample entry (one column per line):
+             1
+             1
+             1
+             Django Application Data
+             192.168.1.179
+             5432
+             app_data
+             uwsgi
+             Django interface server
+             app_data
+             1
+             {"sslmode": "require",
+              "passfile": "/pgpass",
+              "connect_timeout": 10}
+           Notes:
+             - Add one entry for each front end database
+
+        e. sharedserver: Non-admin user entries for local postgres server and
+        databases (pointing back to "server")
+           Table structure (required columns):
+             id INTEGER NOT NULL
+               Auto generated primary key
+             user_id INTEGER NOT NULL
+             server_owner VARCHAR(64)
+             servergroup_id INTEGER NOT NULL
+             name VARCHAR(128) NOT NULL
+             host VARCHAR(128)
+             port INTEGER
+             maintenance_db VARCHAR(64)
+             username VARCHAR(64)
+             shared BOOLEAN NOT NULL
+             osid INTEGER
+             connection_params JSON
+             FOREIGN KEY(servergroup_id) REFERENCES servergroup (id)
+             FOREIGN KEY(user_id) REFERENCES user (id)
+           Sample entry (one column per line):
+             1
+             2
+             pgadmin@laikasden.com
+             1
+             Django Application Data
+             192.168.1.179
+             5432
+             app_data
+             uwsgi
+             1
+             1
+             {"sslmode": "require",
+              "passfile": "/pgpass",
+              "connect_timeout": 10}
+
     The admin user will always exist since it is initialized by the
     docker start-up. However, other table data won't exist at start-up, while
     the admin user password needs to be rotated.
 
     The rules for the update are as follows:
 
-    1. If the user doesn't exist, add entries to database tables in the
-    following order:
-       a. user
+    1. User doesn't exist
+       - Add appropriate entries to database tables following the table order
+       shown above.
+       - On initial entry of non-admin user, the password is randomly
+       generated and won't be saved. This will necessitate some password
+       change utility on the control panel that allows the user to reset
+       his/her password, and this utility will need to hook into
+       the padmin database to reset the password there as well. See the
+       utility function above, "set_pgadmin_password".
+    2. User exists, admin user
+       - Verify all entries exist for all tables
+       - Rotate user password
+       - Put copy of password in admin's directory, if exists.
+    3. User exists, non-admin user
+       - Verify all entries exist for all tables
 
     """
     pgadmin_db = (__salt__['pillar.get']('pgadmin_lib_path')
@@ -224,6 +316,7 @@ def pgadmin_db_user(username, email_address):
     connection = sqlite3.connect(pgadmin_db)
     cursor = connection.cursor()
 
+    # Temporary file write for development
     f = open("/tmp/pgadmin.txt", "a")
     # Check if user is in database
     query = ("select * from user where email = '{}'"
