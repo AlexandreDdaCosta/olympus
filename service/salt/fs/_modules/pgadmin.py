b7dcdde4a9c19a0c085117072e0a36ff4151a6a0
Alexandre da Costa <alexandre.dias.dacosta@gmail.com>
2023-07-22 21:53:56 -0400

    Testing pgadmin invalid user delete.

--- service/salt/fs/_modules/pgadmin.py
+++ service/salt/fs/_modules/pgadmin.py
@@ -147,7 +147,7 @@ def remove_invalid_users():
                 if email_address == directory:
                     f.write("Printing user and is_staff\n")
                     f.write(user + "\n")
-                    f.write(users[user]['is_staff'] + "\n")
+                    f.write(str(users[user]['is_staff']) + "\n")
                     if user == pgadmin_default_user or users[user]['is_staff']:
                         valid_user = True
                     break
